e63adc0a3d5dfcbc762a0f4c7bfee9f929046934
Henrik Lissner <henrik@lissner.net>
2018-08-16 14:55:49 +0200

    Rewrite highlight-escape-sequences.el
    + Add support for more languages:
      + python-mode
      + scala-mode
      + typescript-mode
      + enh-ruby-mode
    + Adds a fallback rule for unsupported major modes.
    + Adds a toggle-able buffer-local minor
      mode (`highlight-escape-sequences-mode`) and a global
      one (`global-highlight-escape-sequences-mode`).
    + Improves Elisp escape sequence highlighting to support modifier+key
      sequences and (soon) magic regexp symbols.
    + Fixes how font-lock keywords are applied (now they are applied
      immediately to the current buffer; you no longer need to restart
      buffers if you enable the plugin later/lazy load it).
    + Byte-compile regexps built with rx.

--- highlight-escape-sequences.el
+++ highlight-escape-sequences.el
@@ -3,10 +3,13 @@
 ;; Copyright (C) 2013, 2015-2017  Free Software Foundation, Inc.
 
 ;; Author:   Dmitry Gutov <dgutov@yandex.ru>
-;;	Pavel Matcula <dev.plvlml@gmail.com>
-;; URL:      https://github.com/dgutov/highlight-escape-sequences
+;;	         Pavel Matcula <dev.plvlml@gmail.com>
+;;           Henrik Lissner <henrik@lissner.net>
+;; Maintainer: Henrik Lissner <henrik@lissner.net>
 ;; Keywords: convenience
-;; Version:  0.4
+;; Version:  0.5
+;; Homepage: https://github.com/hlissner/evil-snipe
+;; Package-Requires: ((emacs "24.4"))
 
 ;; This file is part of GNU Emacs.
 
@@ -40,30 +43,40 @@
 
 ;; Put this in the init file:
 ;;
-;; (hes-mode)
+;; (global-highlight-escape-sequences-mode +1)
 
 ;;; Code:
+
+(eval-when-compile (require 'rx))
 
 (defgroup hes-mode nil
   "Highlight escape sequences."
   :group 'convenience)
 
 (defface hes-escape-backslash-face
-  '((default :inherit font-lock-regexp-grouping-backslash))
-  "Face to highlight an escape backslash.")
+  '((t :inherit font-lock-regexp-grouping-backslash))
+  "Face to highlight an escape backslash."
+  :group 'hes-mode)
 
 (defface hes-escape-sequence-face
-  '((default :inherit font-lock-regexp-grouping-construct))
-  "Face to highlight an escape sequence.")
+  '((t :inherit font-lock-regexp-grouping-construct))
+  "Face to highlight an escape sequence."
+  :group 'hes-mode)
 
-(defconst hes-common-escape-sequence-re
-  (rx (submatch
-       (and ?\\ (submatch
-                 (or (repeat 1 3 (in "0-7"))
-                     (and ?x (repeat 2 xdigit))
-                     (and ?u (repeat 4 xdigit))
-                     (any "\"\'\\bfnrtv"))))))
-  "Regexp to match the most common escape sequences.
+
+;;
+;; Escape sequence regexps
+;;
+
+(defvar hes-fallback-escape-sequence-re
+  (eval-when-compile
+    (rx (submatch
+         (and ?\\ (submatch
+                   (or (repeat 1 3 (in "0-7"))
+                       (and ?x (repeat 2 xdigit))
+                       (and ?u (repeat 4 xdigit))
+                       (any "\"\'\\bfnrtv")))))))
+  "Fallback regexp to match the most common escape sequences.
 
 Currently handles:
 - octals (\\0 to \\777),
@@ -71,14 +84,15 @@ Currently handles:
 - unicodes (\\u0000 to \\uFFFF),
 - and backslash followed by one of \"\'\\bfnrtv.")
 
-(defconst hes-c/c++/objc-escape-sequence-re
-  (rx (submatch
-       (and ?\\ (submatch
-                 (or (repeat 1 3 (in "0-7"))
-                     (and ?x (1+ xdigit))
-                     (and ?u (repeat 4 xdigit))
-                     (and ?U (repeat 8 xdigit))
-                     (any "\"\'\?\\abfnrtv"))))))
+(defvar hes-c/c++/objc-escape-sequence-re
+  (eval-when-compile
+    (rx (submatch
+         (and ?\\ (submatch
+                   (or (repeat 1 3 (in "0-7"))
+                       (and ?x (1+ xdigit))
+                       (and ?u (repeat 4 xdigit))
+                       (and ?U (repeat 8 xdigit))
+                       (any "\"\'\?\\abfnrtv")))))))
   "Regexp to match C/C++/ObjC escape sequences.
 
 Currently handles:
@@ -87,12 +101,13 @@ Currently handles:
 - unicodes (\\u0000 to \\uFFFF, \\U00000000 to \\UFFFFFFFF),
 - and backslash followed by one of \"\'\?\\abfnrtv.")
 
-(defconst hes-java-escape-sequence-re
-  (rx (submatch
-       (and ?\\ (submatch
-                 (or (repeat 1 3 (in "0-7"))
-                     (and ?u (repeat 4 xdigit))
-                     (any "\"\'\\bfnrt"))))))
+(defvar hes-java-escape-sequence-re
+  (eval-when-compile
+    (rx (submatch
+         (and ?\\ (submatch
+                   (or (repeat 1 3 (in "0-7"))
+                       (and ?u (repeat 4 xdigit))
+                       (any "\"\'\\bfnrt")))))))
   "Regexp to match Java escape sequences.
 
 Currently handles:
@@ -100,14 +115,15 @@ Currently handles:
 - unicodes (\\u0000 to \\uFFFF),
 - and backslash followed by one of \"\'\\bfnrt.")
 
-(defconst hes-js-escape-sequence-re
-  (rx (submatch
-       (and ?\\ (submatch
-                 (or (repeat 1 3 (in "0-7"))
-                     (and ?x (repeat 2 xdigit))
-                     (and ?u (repeat 4 xdigit))
-                     ;; (any "\"\'\\bfnrtv")
-                     not-newline))))) ;; deprecated
+(defvar hes-js-escape-sequence-re
+  (eval-when-compile
+    (rx (submatch
+         (and ?\\ (submatch
+                   (or (repeat 1 3 (in "0-7"))
+                       (and ?x (repeat 2 xdigit))
+                       (and ?u (repeat 4 xdigit))
+                       ;; (any "\"\'\\bfnrtv")
+                       not-newline)))))) ;; deprecated
   "Regexp to match JavaScript escape sequences.
 
 Currently handles:
@@ -116,19 +132,20 @@ Currently handles:
 - unicodes (\\u0000 to \\uFFFF),
 - and backslash followed by anything else.")
 
-(defconst hes-ruby-escape-sequence-re
-  (rx (submatch
-       (and ?\\ (submatch
-                 (or (repeat 1 3 (in "0-7"))
-                     (and ?x (repeat 1 2 xdigit))
-                     (and ?u
-                          (or (repeat 4 xdigit)
-                              (and ?{
-                                   (repeat 1 6 xdigit)
-                                   (0+ (1+ space)
-                                       (repeat 1 6 xdigit))
-                                   ?})))
-                     not-newline)))))
+(defvar hes-ruby-escape-sequence-re
+  (eval-when-compile
+    (rx (submatch
+         (and ?\\ (submatch
+                   (or (repeat 1 3 (in "0-7"))
+                       (and ?x (repeat 1 2 xdigit))
+                       (and ?u
+                            (or (repeat 4 xdigit)
+                                (and ?{
+                                     (repeat 1 6 xdigit)
+                                     (0+ (1+ space)
+                                         (repeat 1 6 xdigit))
+                                     ?})))
+                       not-newline))))))
   "Regexp to match Ruby escape sequences.
 
 Currently handles:
@@ -140,7 +157,7 @@ Currently handles:
 
 Currently doesn't handle \\C-, \\M-, etc.")
 
-(defconst hes-ruby-escape-sequence-keywords
+(defvar hes-ruby-escape-sequence-keywords
   `((,hes-ruby-escape-sequence-re
      (0 (let* ((state (syntax-ppss))
                (term (nth 3 state)))
@@ -149,8 +166,8 @@ Currently doesn't handle \\C-, \\M-, etc.")
                     (if (fboundp 'ruby-syntax-expansion-allowed-p)
                         (ruby-syntax-expansion-allowed-p state)
                       (memq term '(?\" ?/ ?\n ?` t))))
-            ;; TODO: Switch to `add-face-text-property' when we're
-            ;; fine with only supporting Emacs 24.4 and up.
+            ;; TODO: Switch to `add-face-text-property' when we're fine with
+            ;; only supporting Emacs 24.4 and up.
             (font-lock-prepend-text-property (match-beginning 1) (match-end 1)
                                              'face 'hes-escape-backslash-face)
             (font-lock-prepend-text-property (match-beginning 2) (match-end 2)
@@ -158,24 +175,72 @@ Currently doesn't handle \\C-, \\M-, etc.")
             nil))
         prepend))))
 
-(defconst hes-elisp-escape-sequence-re
-  (rx (submatch
-       (and ?\\ (submatch
-                 (or
-                  (and ?u (repeat 4 xdigit))
-                  (and ?U ?0 ?0 (repeat 6 xdigit))
-                  (and ?x (+ xdigit)) ;; variable number hex digits
-                  (+ (in "0-7"))      ;; variable number octal digits
-                  not-newline)))))
-
+(defvar hes-elisp-escape-sequence-re
+  (eval-when-compile
+    (rx (submatch
+         (and ?\\
+              (submatch
+               (or (and ?u (repeat 4 xdigit))
+                   (and ?U ?0 ?0 (repeat 6 xdigit))
+                   (and ?x (+ xdigit)) ; variable number hex digits
+                   (and (or ?C ?S ?A) ?- not-newline)  ; modifier & key escape codes
+                   (+ (in "0-7"))      ; variable number octal digits
+                   ;; TODO Add magic regexp symbols?
+                   not-newline))))))
   "Regexp to match Emacs Lisp escape sequences.
 
 Currently handles:
 - unicodes (\\uNNNN and \\U00NNNNNN)
 - hexadecimal (\\x...) and octal (\\0-7), variable number of digits
+- modifier + key (\\C-c)
 - backslash followed by anything else.")
 
-(defun hes-make-simple-escape-sequence-keywords(re)
+(defvar hes-python-escape-sequence-re
+  (eval-when-compile
+    (rx (submatch
+         (and ?\\ (submatch
+                   (or (repeat 1 3 (in "0-7"))
+                       (and ?x (repeat 2 xdigit))
+                       (and ?u (repeat 4 xdigit))
+                       (and ?U (repeat 8 xdigit))
+                       (and ?N "{" (one-or-more alpha) "}")
+                       (any "\"\'\\abfnrtv")))))))
+  "Regexp to match Python escape sequences.")
+
+;;
+(defcustom hes-mode-alist
+  `((c-mode          . hes-c/c++/objc-escape-sequence-re)
+    (c++-mode        . hes-c/c++/objc-escape-sequence-re)
+    (objc-mode       . hes-c/c++/objc-escape-sequence-re)
+    (go-mode         . hes-c/c++/objc-escape-sequence-re)
+    (java-mode       . hes-java-escape-sequence-re)
+    (clojure-mode    . hes-java-escape-sequence-re)
+    (scala-mode      . hes-java-escape-sequence-re)
+    (js-mode         . hes-js-escape-sequence-re)
+    (js2-mode        . hes-js-escape-sequence-re)
+    (typescript-mode . hes-js-escape-sequence-re)
+    (coffee-mode     . hes-js-escape-sequence-re)
+    (ruby-mode       . hes-ruby-escape-sequence-keywords)
+    (enh-ruby-mode   . hes-ruby-escape-sequence-keywords)
+    (python-mode     . hes-python-escape-sequence-keywords)
+    (emacs-lisp-mode . hes-elisp-escape-sequence-re)
+    (t . hes-fallback-escape-sequence-re))
+  "An alist mapping major modes to font-lock rules.
+
+These rules can either a regexp, `font-lock-keywords', or symbols named after
+variables that contains one of the aforementioned types.
+
+If there is no matching major mode, fall back to the entry whose CAR is t."
+  :type '(repeat function)
+  :set (lambda (symbol value)
+         (if (bound-and-true-p highlight-escape-sequences-mode)
+             (progn
+               (highlight-escape-sequences-mode -1)
+               (set-default symbol value)
+               (highlight-escape-sequences-mode 1))
+           (set-default symbol value))))
+
+(defun hes--build-escape-sequence-keywords (re)
   `((,re
      (1 (when (nth 3 (syntax-ppss))
           'hes-escape-backslash-face)
@@ -184,69 +249,39 @@ Currently handles:
           'hes-escape-sequence-face)
         prepend))))
 
-(defcustom hes-mode-alist
-  `((c-mode          . ,hes-c/c++/objc-escape-sequence-re)
-    (c++-mode        . ,hes-c/c++/objc-escape-sequence-re)
-    (objc-mode       . ,hes-c/c++/objc-escape-sequence-re)
-    (go-mode         . ,hes-c/c++/objc-escape-sequence-re)
-    (java-mode       . ,hes-java-escape-sequence-re)
-    (clojure-mode    . ,hes-java-escape-sequence-re)
-    (js-mode         . ,hes-js-escape-sequence-re)
-    (js2-mode        . ,hes-js-escape-sequence-re)
-    (ruby-mode       . ,hes-ruby-escape-sequence-keywords)
-    (emacs-lisp-mode . ,hes-elisp-escape-sequence-re))
-  "Alist of regexps or `font-lock-keywords' elements for major modes."
-  :type '(repeat function)
-  :set (lambda (symbol value)
-         (if (bound-and-true-p hes-mode)
-             (progn
-               (hes-mode -1)
-               (set-default symbol value)
-               (hes-mode 1))
-           (set-default symbol value))))
-
 ;;;###autoload
-(defun turn-on-hes-mode()
-  "Turn on highlighting of escape sequences."
+(defun turn-on-hes-mode ()
+  "Turn on `highlight-escape-sequences-mode'."
   (interactive)
-  (dolist (mode hes-mode-alist)
-    (if (atom mode)
-        (font-lock-add-keywords
-         mode
-         (hes-make-simple-escape-sequence-keywords hes-common-escape-sequence-re)
-         'append)
-      (when (stringp (cdr mode))
-        (font-lock-add-keywords
-         (car mode)
-         (hes-make-simple-escape-sequence-keywords (cdr mode))
-         'append))
-      (when (listp (cdr mode))
-        (font-lock-add-keywords (car mode) (cdr mode) 'append)))))
+  (highlight-escape-sequences-mode +1))
 
 ;;;###autoload
-(defun turn-off-hes-mode()
-  "Turn off highlighting of escape sequences"
+(defun turn-off-hes-mode ()
+  "Turn off `highlight-escape-sequences-mode'."
   (interactive)
-  (dolist (mode hes-mode-alist)
-    (if (atom mode)
-        (font-lock-remove-keywords
-         mode
-         (hes-make-simple-escape-sequence-keywords hes-common-escape-sequence-re))
-      (when (stringp (cdr mode))
-        (font-lock-remove-keywords
-         (car mode)
-         (hes-make-simple-escape-sequence-keywords (cdr mode))))
-      (when (listp (cdr mode))
-        (font-lock-remove-keywords (car mode) (cdr mode))))))
+  (highlight-escape-sequences-mode -1))
 
 ;;;###autoload
-(define-minor-mode hes-mode
-  "Toggle highlighting of escape sequences."
-  :lighter "" :global t
-  (if hes-mode
-      (turn-on-hes-mode)
-    (turn-off-hes-mode)))
+(define-minor-mode highlight-escape-sequences-mode
+  "Toggle highlighting of escape sequences in the current buffer."
+  :lighter ""
+  :init-value nil
+  (let* ((var (cdr (or (assq major-mode hes-mode-alist)
+                       (assq 't hes-mode-alist))))
+         (keywords
+          (cond ((or (stringp var) (listp var)) var)
+                ((symbolp var) (symbol-value var))
+                ((error "Unexpected value in `hes-mode-alist': %s" var)))))
+    (when keywords
+      (when (stringp keywords)
+        (setq keywords (hes--build-escape-sequence-keywords keywords)))
+      (if highlight-escape-sequences-mode
+          (font-lock-add-keywords nil keywords)
+        (font-lock-remove-keywords nil keywords)))))
+
+;;;###autoload
+(define-globalized-minor-mode global-highlight-escape-sequences-mode
+  highlight-escape-sequences-mode turn-on-hes-mode)
 
 (provide 'highlight-escape-sequences)
-
 ;;; highlight-escape-sequences.el ends here
